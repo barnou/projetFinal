f7df47da191960443d476b84f3d0886c494b0772
baptiste <b.arnou05@gmail.com>
2013-02-27 11:49:14 +0100

    Fin des pages statiques

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,18 +1,44 @@
 require 'spec_helper'
 
 describe PagesController do
+	render_views
 
   describe "GET 'home'" do
     it "should be successful" do
-      get 'home'
+      get :home
       response.should be_success
+    end
+    
+    it "devrait avoir le bon titre" do
+			get :home
+			response.should have_selector("title",
+													:content => "Projet RoR Arnou Baptiste | Accueil")
     end
   end
 
   describe "GET 'contact'" do
     it "should be successful" do
-      get 'contact'
+      get :contact
       response.should be_success
+    end
+    
+    it "devrait avoir le bon titre" do
+			get :contact
+			response.should have_selector("title",
+													:content => "Projet RoR Arnou Baptiste | Contact")
+    end
+  end
+  
+  describe "GET 'about'" do
+		it "devrait reussir" do
+			get :about
+			response.should be_success
+		end
+		
+		it "devrait avoir le bon titre" do
+			get :about
+			response.should have_selector("title",
+													:content => "Projet RoR Arnou Baptiste | A propos")
     end
   end
 
