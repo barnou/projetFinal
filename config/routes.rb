7f2762982e02580a49fe60b5766c6e67c0c797b3
baptiste <b.arnou05@gmail.com>
2013-02-27 11:09:30 +0100

    Ajout d'un controleur Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ProjetArnouBaptiste::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
