0724e7d8d9f42abc5b07f49213a3e58564699b0d
baptiste <b.arnou05@gmail.com>
2013-02-27 10:57:06 +0100

    Depot initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjetArnouBaptiste::Application.config.secret_token = 'f486a92d462e472f1e720cf0b78fbe38b7d55a5826693d4ef21ce2a1c6f1ac3d880a52e9506a7bb3501d93394fde52cfc6bceee030f2e85282237570edbf6b2b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ProjetArnouBaptiste::Application.config.session_store :cookie_store, :key => '_projetArnouBaptiste_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ProjetArnouBaptiste::Application.config.session_store :active_record_store
